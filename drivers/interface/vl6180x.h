daac2dcd3ad74c31a57531449b143a50c3960cc9
mwall002 <mwall002@gmail.com>
2015-08-30 15:52:27 -0700

    Create vl6180x.h

---
+++ drivers/interface/vl6180x.h
@@ -0,0 +1,140 @@
+/**
+ *    ||          ____  _ __
+ * +------+      / __ )(_) /_______________ _____  ___
+ * | 0xBC |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
+ * +------+    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
+ *  ||  ||    /_____/_/\__/\___/_/   \__,_/ /___/\___/
+ *
+ * Crazyflie Firmware
+ *
+ * Copyright (C) 2011-2012 Bitcraze AB
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, in version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @file eeprom.h
+ * Driver for the 24AA64F eeprom.
+ *
+ */
+#ifndef vl6180x_h
+#define vl6180x_h
+
+#include <stdbool.h>
+#include "i2cdev.h"
+
+#define VL6180X_I2C_ADDR     0x29
+#define VL6180X_SIZE         0x1FFF
+
+#define VL6180x_FAILURE_RESET  -1
+
+#define VL6180X_IDENTIFICATION_MODEL_ID              0x0000
+#define VL6180X_IDENTIFICATION_MODEL_REV_MAJOR       0x0001
+#define VL6180X_IDENTIFICATION_MODEL_REV_MINOR       0x0002
+#define VL6180X_IDENTIFICATION_MODULE_REV_MAJOR      0x0003
+#define VL6180X_IDENTIFICATION_MODULE_REV_MINOR      0x0004
+#define VL6180X_IDENTIFICATION_DATE                  0x0006 //16bit value
+#define VL6180X_IDENTIFICATION_TIME                  0x0008 //16bit value
+
+#define VL6180X_SYSTEM_MODE_GPIO0                    0x0010
+#define VL6180X_SYSTEM_MODE_GPIO1                    0x0011
+#define VL6180X_SYSTEM_HISTORY_CTRL                  0x0012
+#define VL6180X_SYSTEM_INTERRUPT_CONFIG_GPIO         0x0014
+#define VL6180X_SYSTEM_INTERRUPT_CLEAR               0x0015
+#define VL6180X_SYSTEM_FRESH_OUT_OF_RESET            0x0016
+#define VL6180X_SYSTEM_GROUPED_PARAMETER_HOLD        0x0017
+
+#define VL6180X_SYSRANGE_START                       0x0018
+#define VL6180X_SYSRANGE_THRESH_HIGH                 0x0019
+#define VL6180X_SYSRANGE_THRESH_LOW                  0x001A
+#define VL6180X_SYSRANGE_INTERMEASUREMENT_PERIOD     0x001B
+#define VL6180X_SYSRANGE_MAX_CONVERGENCE_TIME        0x001C
+#define VL6180X_SYSRANGE_CROSSTALK_COMPENSATION_RATE 0x001E
+#define VL6180X_SYSRANGE_CROSSTALK_VALID_HEIGHT      0x0021
+#define VL6180X_SYSRANGE_EARLY_CONVERGENCE_ESTIMATE  0x0022
+#define VL6180X_SYSRANGE_PART_TO_PART_RANGE_OFFSET   0x0024
+#define VL6180X_SYSRANGE_RANGE_IGNORE_VALID_HEIGHT   0x0025
+#define VL6180X_SYSRANGE_RANGE_IGNORE_THRESHOLD      0x0026
+#define VL6180X_SYSRANGE_MAX_AMBIENT_LEVEL_MULT      0x002C
+#define VL6180X_SYSRANGE_RANGE_CHECK_ENABLES         0x002D
+#define VL6180X_SYSRANGE_VHV_RECALIBRATE             0x002E
+#define VL6180X_SYSRANGE_VHV_REPEAT_RATE             0x0031
+
+#define VL6180X_SYSALS_START                         0x0038
+#define VL6180X_SYSALS_THRESH_HIGH                   0x003A
+#define VL6180X_SYSALS_THRESH_LOW                    0x003C
+#define VL6180X_SYSALS_INTERMEASUREMENT_PERIOD       0x003E
+#define VL6180X_SYSALS_ANALOGUE_GAIN                 0x003F
+#define VL6180X_SYSALS_INTEGRATION_PERIOD            0x0040
+
+#define VL6180X_RESULT_RANGE_STATUS                  0x004D
+#define VL6180X_RESULT_ALS_STATUS                    0x004E
+#define VL6180X_RESULT_INTERRUPT_STATUS_GPIO         0x004F
+#define VL6180X_RESULT_ALS_VAL                       0x0050
+#define VL6180X_RESULT_HISTORY_BUFFER                0x0052
+#define VL6180X_RESULT_RANGE_VAL                     0x0062
+#define VL6180X_RESULT_RANGE_RAW                     0x0064
+#define VL6180X_RESULT_RANGE_RETURN_RATE             0x0066
+#define VL6180X_RESULT_RANGE_REFERENCE_RATE          0x0068
+#define VL6180X_RESULT_RANGE_RETURN_SIGNAL_COUNT     0x006C
+#define VL6180X_RESULT_RANGE_REFERENCE_SIGNAL_COUNT  0x0070
+#define VL6180X_RESULT_RANGE_RETURN_AMB_COUNT        0x0074
+#define VL6180X_RESULT_RANGE_REFERENCE_AMB_COUNT     0x0078
+#define VL6180X_RESULT_RANGE_RETURN_CONV_TIME        0x007C
+#define VL6180X_RESULT_RANGE_REFERENCE_CONV_TIME     0x0080
+
+#define VL6180X_READOUT_AVERAGING_SAMPLE_PERIOD      0x010A
+#define VL6180X_FIRMWARE_BOOTUP                      0x0119
+#define VL6180X_FIRMWARE_RESULT_SCALER               0x0120
+#define VL6180X_I2C_SLAVE_DEVICE_ADDRESS             0x0212
+#define VL6180X_INTERLEAVED_MODE_ENABLE              0x02A3
+
+//#define VL6180X_ADDRESS 0x29// 0x52
+
+#define RANGING_CYCLE_MS	80
+
+/**
+ * Initialize the i2c eeprom module
+ * @param i2cPort  I2C port ( a CPAL_InitTypeDef) the eeprom is connected to.
+ *
+ * @return True on success, else false.
+ */
+bool vl6180xInit(I2C_Dev *i2cPort);//I2C_Dev *i2cPort
+
+/**
+ * Test that the eeprom is there
+ *
+ * @return True on success, else false.
+ */
+bool vl6180xTest(void);
+
+/**
+ * Test that the eeprom is there
+ *
+ * @return True on success, else false.
+ */
+bool vl6180xTestConnection(void);
+
+
+void VL6180xDefaultSettings(void);
+
+uint8_t vl6180xgetDistance();
+
+//void getAmbientLight();
+
+//bool vl6180xReadBuffer(uint8_t* buffer, uint16_t readAddr, uint16_t len);
+//
+//bool vl6180xWriteBuffer(uint8_t* buffer, uint16_t writeAddr, uint16_t len);
+//
+//
+//bool vl6180xWritePage(uint8_t* buffer, uint16_t writeAddr);
+
+#endif // EERROM_H
